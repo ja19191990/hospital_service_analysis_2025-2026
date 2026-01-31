0fac34ac665d01d58efcbe6b52b761ce02ee615b
Juan Alva <164815268+ja19191990@users.noreply.github.com>
2026-01-31 17:01:31 -0600

    Analytical KPIs added
    Queries to get KPIs with SQL engine. 2026-01-31.

---
+++ sql/dml/kpi_largest_positive_difference.sql
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- KPI Which 5 questions had the largest average postive difference?
+-- KPI ¿Cúales 5 preguntas tuvieron la mayor diferencia positiva promedio?
+-----------------------------------------------------------------------------------------------------------------------
+
+SELECT
+    pregunta,
+    ROUND(AVG(diferencia), 3) AS average_difference
+FROM satisfaccion
+WHERE diferencia >= 1
+GROUP BY pregunta
+ORDER BY ROUND(AVG(diferencia), 3) DESC
+LIMIT 5;
---
+++ sql/dml/kpi_patients_per_procedure.sql
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- KPI How many patients were per procedure?
+-- KPI ¿Cuantos pacientes hubo por procedimiento?
+-----------------------------------------------------------------------------------------------------------------------
+
+SELECT
+    procedimiento,
+    COUNT(encuesta) AS total_pacientes
+FROM generales
+GROUP BY procedimiento
+ORDER BY total_pacientes DESC;
