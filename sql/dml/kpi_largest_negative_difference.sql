94bf8c5bcbc7b2e92f6085b46e2f775f701c5bbf
Juan Alva <164815268+ja19191990@users.noreply.github.com>
2026-02-07 18:03:44 -0600

    KPI sql script added with correction in header
    2026-02-06.

---
+++ sql/dml/kpi_largest_negative_difference.sql
@@ -0,0 +1,13 @@
+------------------------------------------------------------------------------------------------
+-- KPI Which 5 questions had the largest average negative difference?
+-- KPI ¿Cúales 5 preguntas tuvieron la mayor diferencia negativa promedio?
+------------------------------------------------------------------------------------------------
+
+SELECT
+    pregunta,
+    ROUND(AVG(diferencia), 3) AS average_difference
+FROM satisfaccion
+WHERE diferencia < 0
+GROUP BY pregunta
+ORDER BY ROUND(AVG(diferencia), 3) ASC
+LIMIT 5;
